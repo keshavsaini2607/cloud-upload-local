f3fe1d0236feb44b2e330b3b33e4478d7df3e027
keshavsaini <skeshav162@gmail.com>
2023-10-02 15:25:00 +0530

    error handelled

--- src/buckets/schema/bucket.schema.ts
+++ src/buckets/schema/bucket.schema.ts
@@ -11,7 +11,7 @@ export class Bucket {
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ required: false })
   description: string;
 
   @Prop({ required: true })
